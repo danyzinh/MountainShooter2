3e4d61f8c8077d70835a54f599b4ca1982dbaefb
danyzinh <danielasouzadamasceno@gmail.com>
2026-07-01 21:14:12 -0300

    Creating main loop Connecting Menu and Game classes

--- code/game.py
+++ code/game.py
@@ -1,9 +1,44 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*
+
+# Importando o Pygame
+import pygame
+
+from code.menu import Menu
+
 
 class Game:
     def __init__(self):
-        self.window = None
+
+        # Comando para inicializar o pygame
+        pygame.init()
+
+        # Variável para criar Janela
+        self.window = pygame.display.set_mode(size=(600, 480))
 
     def run(self, ):
-        pass
+
+        # Loop para manter a janela aberta
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+            pass
+
+
+
+
+            ''' Cheque por todos os eventos (check for all events)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit() # Fecha janela
+                    quit() # Finaliza o pygame '''
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,22 +1,10 @@
-# Importando o Pygame
-import pygame
+from code.game import Game
 
-# Começando o código
-print('Setup Start')
+# Variável para chamar a classe Game
 
-# Comando para inicializar o pygame
-pygame.init()
+game = Game()
+game.run()
 
-# Variável para criar Janela
-window = pygame.display.set_mode(size = (600, 480))
-
-# Loop para manter a janela aberta
-while True:
-    # Cheque por todos os eventos (check for all events)
-    for event in pygame.event.get(): # Vai checar eventos
-        if event.type == pygame.QUIT: # Sair
-            pygame.quit() # Fechar Janela
-            quit() # Encerrar a inicialização do Pygame
 
 
 
