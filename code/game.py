3fd2e5f72ab8a91fa1093f7d2144e38012bef865
danyzinh <danielasouzadamasceno@gmail.com>
2026-07-21 23:57:17 -0300

    Level 2 implementation

--- code/game.py
+++ code/game.py
@@ -20,14 +20,19 @@ class Game:
 
     def run(self, ):
 
-        # Loop para manter a janela aberta
+        # ‘Loop’ para manter a janela aberta
         while True:
             menu = Menu(self.window)
             menu_return = menu.run()
 
             if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
-                level = Level(self.window, 'Level1', menu_return)
-                level_return = level.run()
+                player_score = [0, 0] # [Player1, Player2]
+                level = Level(self.window, 'Level1', menu_return, player_score)
+                level_return = level.run(player_score)
+                if level_return:
+                    level = Level(self.window, 'Level2', menu_return, player_score)
+                    level_return = level.run(player_score)
+
 
 
             elif menu_return == MENU_OPTION[4]:
